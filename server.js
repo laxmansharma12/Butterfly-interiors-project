ba2adb7115ac42210ab20fb8b3364ee3435a2095
laxman sharma <91863813+laxmansharma12@users.noreply.github.com>
2023-08-31 19:03:33 +0530

    Update

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import authRoutes from "./routes/authRoute.js";
 import colors from "colors";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from "url";
 
 //confugure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//es module fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
