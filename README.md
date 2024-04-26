a8dfb7691c47fb79a7137683b24e986ad7b0effc
laxman <laxmansharmawe@gmail.com>
2024-04-26 22:36:49 +0530

    made mobile responsive

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Butterfly-interiors-project
-college web development project for 6th semester
-![home-page](https://github.com/laxmansharma12/Butterfly-interiors-project/assets/91863813/5e7b2fa5-2cc1-41c1-950d-6fbe3f13decf)
+# Butterfly-Interiors-vercel-
--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-// const express= require("express");
-// const color=require("colors");
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-import colors from "colors";
 import cors from "cors";
-import path from "path";
-import {fileURLToPath} from "url";
 
 //confugure env
 dotenv.config();
 
 //database config
 connectDB();
-
-//es module fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,18 +18,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 
 //rest api
-app.use("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-})
+app.get("/", (req, res) => {
+	res.send("<h1>Welcome to butterfly interiors</h1>");
+});
 
 //PORT
 const PORT = process.env.PORT || 8080;
 //run listen
 app.listen(PORT, (req, res) => {
+	console.log(`Server runnimg on ${PORT}`);
 });
