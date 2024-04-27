139d13e8a958923c8b9131322973adc76fd213e4
laxman <laxmansharmawe@gmail.com>
2024-04-27 10:43:27 +0530

    added route to client build folder

--- server.js
+++ server.js
@@ -18,13 +18,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-	res.send("<h1>Welcome to butterfly interiors</h1>");
+app.use("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
